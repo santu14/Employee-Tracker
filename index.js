bf5a1cca1a98fa7577bda9b8597fba20eea5db05
Santiago Solana <santiago.sjs@gmail.com>
2020-09-01 16:03:28 -0400

    Started initial logic for takng in user input through inquirer

--- index.js
+++ index.js
@@ -16,6 +16,53 @@ const connection = mysql.createConnection({
     database: "employeesbd",
 });
 
+// Initial inquirer questions
+const start = () => {
+    inquirer
+    .prompt({
+        type: "list",
+        name: "selection",
+        message: "What would you like to do?",
+        choices: [
+            "View all employees",
+            "View all departments",
+            "View all roles",
+            "Add employee",
+            "Add department",
+            "Add role",
+            "Delete employee",
+        ],
+    })
+    .then((answers) => {
+        
+        console.log(answers);
+        
+        // switch case for each option available that runs our functions
+        switch (answers.selection) {
+            case "view all employees":
+                break;
+                case "View all departments":
+                    break;
+        case "View all roles":
+          break;
+
+        case "Add employee":
+          break;
+        case "Add department":
+          break;
+        case "Add role":
+          break;
+      }
+    });
+};
+
+
+// Functions needed
+//  - Individial view all functions for employees departments and roles
+//      - Use department, role, and manager ids to display correponding data from all 3 tables as our view all employees
+//  - Individial add functions for employees departments and roles
+//      - add function contains a subset of inquirer questions for entering information
+//  - Individial delete functions for employees departments and roles
 
 
 connection.connect(function () {
