04f43b4d07711da3430bc55b4ecb126045675bee
Santiago Solana <santiago.sjs@gmail.com>
2020-09-15 15:32:16 -0400

    Updated scheema with foregn keys and manager table

--- index.js
+++ index.js
@@ -4,48 +4,46 @@ const inquirer = require("inquirer");
 
 // set up my sql connection
 const connection = mysql.createConnection({
-    host: "localhost",
-    
-    port: 8080,
-    
-    // Your username
-    user: "root",
-    
-    // Your password
-    password: "mysqlpassword",
-    database: "employeesbd",
+  host: "localhost",
+
+  port: 8080,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "mysqlpassword",
+  database: "employeesbd",
 });
 
 // Initial inquirer questions
 const start = () => {
-    inquirer
+  inquirer
     .prompt({
-        type: "list",
-        name: "selection",
-        message: "What would you like to do?",
-        choices: [
-            "View all employees",
-            "View all departments",
-            "View all roles",
-            "Add employee",
-            "Add department",
-            "Add role",
-            "Delete employee",
-        ],
+      type: "list",
+      name: "selection",
+      message: "What would you like to do?",
+      choices: [
+        "View all employees",
+        "View all departments",
+        "View all roles",
+        "Add employee",
+        "Add department",
+        "Add role",
+        "Delete employee",
+      ],
     })
     .then((answers) => {
-        
-        console.log(answers);
-        
-        // switch case for each option available that runs our functions
-        switch (answers.selection) {
-            case "view all employees":
-                break;
-                case "View all departments":
-                    break;
+      console.log(answers);
+
+      // switch case for each option available that runs our functions
+      switch (answers.selection) {
+        case "view all employees":
+          break;
+        case "View all departments":
+          break;
         case "View all roles":
           break;
-
         case "Add employee":
           break;
         case "Add department":
@@ -56,14 +54,22 @@ const start = () => {
     });
 };
 
-
 // Functions needed
 //  - Individial view all functions for employees departments and roles
 //      - Use department, role, and manager ids to display correponding data from all 3 tables as our view all employees
+
+const viewAllEmployees = () => {
+
+}
+const viewAllDepartment = () => {
+  
+}
+const viewAllRoles = () => {
+  
+}
 //  - Individial add functions for employees departments and roles
 //      - add function contains a subset of inquirer questions for entering information
 //  - Individial delete functions for employees departments and roles
-
 
 connection.connect(function () {
   console.log("connected as id " + connection.threadId + "\n");
--- scheema.sql
+++ scheema.sql
@@ -5,25 +5,34 @@ CREATE DATABASE employeeDB;
 USE employeeDB;
 
 CREATE TABLE employees (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(45) NULL,
   last_name VARCHAR(45) NULL,
   role_id INT NULL,
   manager_id INT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  FOREIGN KEY (manager_id) REFERENCES manager(id)
 );
 
 CREATE TABLE departments (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   dep_name VARCHAR(45) NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(45) NULL,
   salary DECIMAL(10,4) NULL,
   dep_id INT NULL
+  PRIMARY KEY (id),
+  FOREIGN KEY (dep_id) REFERENCES departments(id)
+);
+
+CREATE TABLE manager (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  managerName VARCHAR(30),
   PRIMARY KEY (id)
 );
 
